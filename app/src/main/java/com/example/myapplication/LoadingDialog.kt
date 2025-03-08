20482b1ae855d003cf6c70ac89496346820a2bd5
Katrina0373 <mocalovak6@gmail.com>
2025-03-08 13:40:26 +0300

    update LoadingDialog

--- app/src/main/java/com/example/myapplication/LoadingDialog.kt
+++ app/src/main/java/com/example/myapplication/LoadingDialog.kt
@@ -1,29 +1,26 @@
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import com.example.myapplication.sendingData
+import com.example.myapplication.ui.theme.MyApplicationTheme
 import kotlinx.coroutines.delay
-
 
 @Composable
 fun LoadingDialog(onDismiss: () -> Unit) {
-    val messages = listOf(
-        "Инициализация...",
-        "Загрузка данных...",
-        "Обработка информации...",
-        "Финализация..."
-    )
+    val maxCountPackages = 6
 
-    var currentMessage by remember { mutableStateOf(messages[0]) }
     var step by remember { mutableStateOf(0) }
 
     LaunchedEffect(Unit) {
-        while (step < messages.size) {
-            delay(1000) // Ждем 1 секунду
-
-            currentMessage = messages[step]
-            step++
-        }
+        sendingData.sendData()
         onDismiss() // Закрываем диалог после последнего шага
     }
 
@@ -38,8 +35,51 @@ fun LoadingDialog(onDismiss: () -> Unit) {
             ) {
                 CircularProgressIndicator()
                 Spacer(modifier = Modifier.height(16.dp))
-                Text(text = currentMessage, style = MaterialTheme.typography.bodyMedium)
+                Text(text = "hi", style = MaterialTheme.typography.bodyMedium)
             }
         }
+    }
+}
+
+@Composable
+fun LoadingDialog(isFinished: Boolean, onDismiss: () -> Unit) {
+
+    AlertDialog(
+        onDismissRequest = { },
+        confirmButton = {
+            if (isFinished) {
+                Button(onClick = onDismiss) {
+                    Text("ОК")
+                }
+            }
+        },
+        title = { Text(if (isFinished) "Готово" else "Отправляем...") },
+        text = {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                if (!isFinished) {
+                    CircularProgressIndicator()
+                } else {
+                    Text("Загрузка завершена!")
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+                Text("Всего пакетов: ${sendingData.allPackeges}")
+                Text("Количество отправленных: ${sendingData.sendingPackeges}")
+                Text("Повторно запрошено: ${sendingData.requestedPackages}")
+            }
+        }
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        var showDialog by remember { mutableStateOf(false) }
+        var isFinished by remember { mutableStateOf(false) }
+        LoadingDialog(isFinished, {showDialog = false})
     }
 }
--- app/src/main/java/com/example/myapplication/SendingData.kt
+++ app/src/main/java/com/example/myapplication/SendingData.kt
@@ -2,17 +2,39 @@ package com.example.myapplication
 
 import android.content.Context
 import android.net.Uri
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import kotlinx.coroutines.delay
+import java.lang.Math.random
 
-class SendingData() {
+object sendingData {
     lateinit var byteArray: ByteArray
+    var allPackeges by mutableIntStateOf(1)
+    var sendingPackeges by mutableIntStateOf(0)
+    var requestedPackages by mutableIntStateOf(0)
 
-    constructor(context: Context, uri: Uri) : this() {
+    fun setData (context: Context, uri: Uri) {
         try {
             byteArray = context.contentResolver.openInputStream(uri)?.use { it.readBytes() }!!
+            allPackeges = 1
+            sendingPackeges = 0
+            requestedPackages = 0
         } catch (e: Exception){
             println(e.message)
         }
+    }
 
+    suspend fun sendData(){
+        sendingPackeges = 0
+        val MaxCountPackages: Int = 5
+        allPackeges = 1 + (random() * MaxCountPackages).toInt()
+        while(sendingPackeges < allPackeges){
+            delay(1000)
+            sendingPackeges++
+        }
 
     }
 }
